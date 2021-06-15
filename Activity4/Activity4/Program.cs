cec1d64f38c35a9c08a3ef32df5d54529c45aeee
Ryan Flatley <22flatlr@elmbrookstudents.org>
2021-06-15 14:00:39 -0500

    GradGift

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -6,19 +6,9 @@ namespace Activity4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Emily Berger");
-            Console.WriteLine("University of Wisconsin-Madison");
-            Console.WriteLine("Hug Coupon");
-            Console.WriteLine("Kyle Hekkers");
-            Console.WriteLine("BCHS");
-            Console.WriteLine("Yes");
-            Console.WriteLine("Anubhav Parida");
-            Console.WriteLine("Brookfield East");
-            Console.WriteLine("Socks");
-            Console.WriteLine("Ryan Flatley");
-            Console.WriteLine("BCHS");
-            Console.WriteLine("A gift card with no money on it");
+          
+           
+            Console.WriteLine("New shoes");
         }
     }
 }
