c5098b68be9b95c3f08520ce5ec7df63687c97af
Emily <eaberger2@wisc.edu>
2021-06-15 14:00:51 -0500

    update

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -12,13 +12,12 @@ namespace Activity4
             Console.WriteLine("Hug Coupon");
             Console.WriteLine("Kyle Hekkers");
             Console.WriteLine("BCHS");
-            Console.WriteLine("Yes");
             Console.WriteLine("Anubhav Parida");
-            Console.WriteLine("Brookfield East");
             Console.WriteLine("Socks");
             Console.WriteLine("Ryan Flatley");
             Console.WriteLine("BCHS");
             Console.WriteLine("A gift card with no money on it");
+            Console.WriteLine("$$$");
         }
     }
 }
