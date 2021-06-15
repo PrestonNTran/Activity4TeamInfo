6c1bf8045c3be372ddbfc2956b47f6646b036823
Kyle Hekkers <kyle.hekkers@gmail.com>
2021-06-15 14:00:31 -0500

    Edited Program.cs

--- Activity4/Activity4/Program.cs
+++ Activity4/Activity4/Program.cs
@@ -15,10 +15,10 @@ namespace Activity4
             Console.WriteLine("Yes");
             Console.WriteLine("Anubhav Parida");
             Console.WriteLine("Brookfield East");
-            Console.WriteLine("Socks");
             Console.WriteLine("Ryan Flatley");
             Console.WriteLine("BCHS");
             Console.WriteLine("A gift card with no money on it");
+            Console.WriteLine("Happiness");
         }
     }
 }
